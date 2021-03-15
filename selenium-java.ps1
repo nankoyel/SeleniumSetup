9474010a35550e36012bbd3689fd6d5b981dabe6
jmasonlee <jmasonlee@gmail.com>
2021-03-15 17:08:47 -0600

    Update selenium-java.ps1

--- selenium-java.ps1
+++ selenium-java.ps1
@@ -1,6 +1,7 @@
 # How to run this file:
 #  PS> iwr -useb https://raw.githubusercontent.com/nankoyel/SeleniumSetup/master/selenium-java.ps1 | iex
 
+iwr -useb cin.st | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev_environments/java.ps1
 choco install selenium
 choco install chromedriver
